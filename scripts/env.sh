0f8f3f39aa319931d52afc1ba7c772b4e2226a1d
Juca Crispim <jucacrispim@gmail.com>
2024-06-24 23:58:06 -0300

    Fix env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -4,3 +4,5 @@ docker run -d -p 27017:27017 mongo:latest
 docker run -d --network=host zookeeper:latest
 docker run -d --network=host rabbitmq:latest
 sleep 3
+
+echo $SECRETS | base64 -d > testdata/secrets.json
