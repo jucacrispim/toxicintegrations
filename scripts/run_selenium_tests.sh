17238308970ed03eea514cc8c7388333706f015e
Juca Crispim <jucacrispim@gmail.com>
2024-06-26 17:02:49 -0300

    Add selenium tests to ci

---
+++ scripts/run_selenium_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export DISPLAY=:99
+Xvfb :99  -ac -screen 0, 1368x768x24 &
+ver=`chromedriver --version | cut -d' ' -f 2 | cut -d'.' -f1`
+export CHROME_VERSION=$ver
+behave tests/functional
+status=$?
+killall Xvfb
+rm -f /tmp/.X99-lock
+exit $status
